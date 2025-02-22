f5f457e9f9b78c73f138a038861ede2015dacbb6
Ahmed Naviwala <154126408+ahmedST247@users.noreply.github.com>
2025-02-22 15:32:15 +0530

    Add files via upload

---
+++ If_elif_else Statement/if_else-if_else.py
@@ -0,0 +1,35 @@
+c = 1000
+d = 5000 
+if c > d:
+    print("c is greater than d")
+elif c == d:
+    print("c and d are equal")
+else:
+    print("d is greater than c") 
+# OutPut :- d is greater than c.
+
+# ----------------------------------------------------
+
+x = 100
+y = 100
+if x > y:
+    print("x is greater than y")
+elif x == y:
+    print("x and y are equal")
+else:
+    print("y is greater then y")
+# OutPut :- x and y are equal.
+
+# -------------------------------------------------------
+
+a = 20
+b = 10
+if a > b:
+   print("a is Greater than b")  # if :- if a's value Greater than b.
+elif a == b:
+    print("a and b is equal")  # elif :- if a and b value are equal.
+else:
+    print("b is Greater than a")  # else :- if b's value Greator than a.
+
+# Current OutPut :- a is greater than b
+# because a's value is more than b
