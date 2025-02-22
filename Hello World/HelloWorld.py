427400e566105a33228cfde76f7899add29033ce
Ahmed Naviwala <154126408+ahmedST247@users.noreply.github.com>
2025-02-22 15:31:53 +0530

    Add files via upload

---
+++ Hello World/HelloWorld.py
@@ -0,0 +1,9 @@
+print("Hello World")  # Hello World program.
+
+
+# ----------------------------------------------------
+
+word = "Hello World" # i made variable of hello world and then printed it.
+print(word)
+
+
